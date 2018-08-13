c37efaf27c0ae250f851a67f4395812017f84927
amitkvikram <amitkvikram@rediffmail.com>
2018-08-13 13:22:38 +0530

    lab2.py updated

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,25 +1,36 @@
 import numpy as np
 import pickle
 
-class timeStamp:
-	def __init__(node, entryTime):
-		self.node = node
-		self.entryTime = entryTime
+class mainListItem:
+    def __init__(self, vehicle_no, start_time, source, departure):
+        self.vehicle_no = vehicle_no
+        self.start_time =start_time
+        self.source = source
+        self.destination = departure
+    def printObject(self):
+        print(self.vehicle_no, self.start_time, self.source, self.destination);
+
+class RoadItem:
+    def __init__(self, vehicle_no, arrival_time, departure_time):
+        self.vehicle_no = vehicle_no
+        self.arrival_time = arrival_time
+        self.departure_time = departure_time
+    def printObject(self):
+        print(self.vehicle_no, self.arrival_time, self.departure_time)
 
 def loadVehicleData():
 	#print "Loading Vehicle Matrix"
-	return np.load("vehicle", allow_pickle = True)
+	return np.array(np.load('vehicle'))
 
 def loadRoadData():
 	#print "Loading Road Matrix"
-	return np.load("road", allow_pickle = True)
+	return np.array(np.load('road')).astype(np.float)
 
 def loadTimeData():
 	#print "Loading Time Data"
-	return np.load("time", allow_pickle = True)
+	return np.array(np.load('time', encoding = 'latin1')).astype(np.float)/60
 
-class Vehicle:
-    
+class Environment:
 	vehicleMat = loadVehicleData()
 	timeMat = loadTimeData()
 	roadMat = loadRoadData()
@@ -37,9 +48,7 @@ class Vehicle:
 		for i in range(0, 5):
 			currentNode = self.vehicleMat[self.vehicleNo, i]
 			entryTime =0 
-
-
-vehicle = Vehicle(0);
+    def processCurrentVehicle()
 
 
 
