6184ec77a2c02762d5d0e2e9c5f43c560e77f5ba
amitkvikram <amitkvikram@rediffmail.com>
2018-08-13 13:40:06 +0530

    lab2.py updated

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
 
-class mainListItem:
+class MainListItem:
     def __init__(self, vehicle_no, start_time, source, departure):
         self.vehicle_no = vehicle_no
         self.start_time =start_time
@@ -34,21 +34,45 @@ class Environment:
 	vehicleMat = loadVehicleData()
 	timeMat = loadTimeData()
 	roadMat = loadRoadData()
+    trafiic = np.zeros(vehicleMat.shape)
+    roadList = [[[] for j in range(roadMat.shape[0])] for k in range(roadMat.shape[1])]
+    mainList = []
 
 	def __init__(self, vehicleNo):
 		self.vehicleNo = vehicleNo
-		print(vehicleMat)
+        //Initialize mainList
+        for i in range(0, time.shape[0]):
+            src = vehicle[i,0]
+            dest = vehicle[i,1]
+            mainList.append(MainListItem(i, timeMat[i, 0], src, dest))
 
 	def calcSpeed(self, x):
 		return np.exp(.5*x)/(1 + np.exp(0.5*x)) + 15/(1 + np.exp(0.5*x))
-
-	def createTimeStamp(self):
-		self.timeStamp = []
-		lastEntryTime = time[self.vehicleNo, 0]
-		for i in range(0, 5):
-			currentNode = self.vehicleMat[self.vehicleNo, i]
-			entryTime =0 
-    def processCurrentVehicle()
+    
+    def createTrafiicDetail():
+        while len(mainList)!=0:
+            mainList.sort(key = lambda x:x.start_time, reverse = True)
+            currObj = mainList.pop()   #Type MainListItem
+            currRoadCarList = roadList[currObj.source][currObj.destination]     #Type: RoadItem
+            #push current Car in current roadList
+            roadList[currObj.source][currObj.destination].append(RoadItem(currObj.vehicle_no,
+                                                                  currObj.start_time, 
+                                                                  currObj.start_time + timeTaken))
+            
+            #update traffic detail
+            traffic[currObj.vehicle_no][0] = traffic[currObj.vehicle_no][0].astype(np.int)+1
+            rootNumber = output[currObj.vehicle_no][0].astype(np.int)
+            output[currObj.vehicle_no][ind1] = currObj.start_time + timeTaken
+            
+            #Update mainList
+            if output[currObj.vehicle_no][0] != 4:
+                st = currObj.start_time + timeTaken
+                mainList.append(mainListItem(currObj.vehicle_no,
+                                st[0],
+                                currObj.destination,
+                                vehicle[currObj.vehicle_no][output[currObj.vehicle_no][0].astype(np.int) + 1]))
+   
+    def processCurrentVehicle():
 
 
 
